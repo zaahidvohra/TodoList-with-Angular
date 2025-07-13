9156d1a2ec9e24ecd943730359fa425a0ca595a6
Zaahid Vohra <67497624+zaahidvohra@users.noreply.github.com>
2025-07-14 03:15:34 +0530

    Backend : Error fix

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); // this is now the pool
+const db = require('../models/db');
 
 // Get all todos
 router.get('/', async (req, res) => {

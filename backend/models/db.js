29058276860e48d80adedaade53a387cf1413292
Zaahid Vohra <67497624+zaahidvohra@users.noreply.github.com>
2025-07-14 03:49:06 +0530

    Backend : Railway setup

--- backend/models/db.js
+++ backend/models/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
